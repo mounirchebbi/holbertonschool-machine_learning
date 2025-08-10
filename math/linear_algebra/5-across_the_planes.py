b08181c04abdc836c4b58b689468b9d0b1cc66bd
mounirchebbi <2157@holbertonschool.com>
2025-08-10 22:12:32 +0100

    add 2d matrices

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for adding two 2D matrices element-wise.
+"""
+
+def add_matrices2D(mat1, mat2):
+    """
+    Add two 2D matrices element-wise
+
+    Args:
+        mat1 (list of lists): First matrix (ints/floats)
+        mat2 (list of lists): Second matrix (ints/floats)
+
+    Returns:
+        list of lists: New matrix with element-wise sums, or None if shapes differ
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
