e2fc061dc94c34e58c75a41614d896eaaff64a80
mounirchebbi <2157@holbertonschool.com>
2025-08-10 22:01:15 +0100

    matrix size

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """
+    Returns the dimensions of a matrix as a list of integers.
+
+    Args:
+        matrix (list): A multi-dimensional list.
+
+    Returns:
+        list: Sizes of the matrix in each dimension.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
