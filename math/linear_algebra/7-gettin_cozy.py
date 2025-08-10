b97a5832e7e22697784ec4e4a47e94ced349d842
mounirchebbi <2157@holbertonschool.com>
2025-08-10 22:47:36 +0100

    concat along axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -23,11 +23,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1[0]) != len(mat2[0]):
             return None
         return [row[:] for row in mat1] + [row[:] for row in mat2]
-    
+
     elif axis == 1:
         # Check row count
         if len(mat1) != len(mat2):
             return None
         return [mat1[i][:] + mat2[i][:] for i in range(len(mat1))]
-    
+
     return None
