cab0be38da1d12255a373041aa7ac160b254817a
mounirchebbi <2157@holbertonschool.com>
2025-08-10 21:51:56 +0100

    list slicing

---
+++ math/linear_algebra/0-slice_me_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for basic Python list slicing.
+
+- arr1 first two elements of arr
+- arr2 last five elements of arr
+- arr3 2nd through 6th elements of arr
+No loops or conditionals are used in this script.
+"""
+
+arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
+arr1 = arr[:2]
+arr2 = arr[-5:]
+arr3 = arr[1:6]
+print("The first two numbers of the array are: {}".format(arr1))
+print("The last five numbers of the array are: {}".format(arr2))
+print("The 2nd through 6th numbers of the array are: {}".format(arr3))
