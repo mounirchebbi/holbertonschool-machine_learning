80a0737cba0139586e35722dc02817b41ec4e76c
mounirchebbi <2157@holbertonschool.com>
2025-08-17 14:44:11 +0100

    stacked bar

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -6,22 +6,25 @@ import matplotlib.pyplot as plt
 def bars():
     """Plot a stacked bar graph of fruit counts per person"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4, 3))  # 4 fruits x 3 people
-    people = ['Farrah', 'Fred', 'Felicia']
-    fruits = ['Apples', 'Bananas', 'Oranges', 'Peaches']
-    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
-    bottoms = np.zeros(fruit.shape[1])
-
-    for i in range(fruit.shape[0]):
-        plt.bar(people, fruit[i], bottom=bottoms, color=colors[i], width=0.5, label=fruits[i])
-        bottoms += fruit[i]
-
-    plt.ylabel("Quantity of Fruit", fontsize='x-small')
-    plt.yticks(np.arange(0, 81, 10), fontsize='x-small')
+    t = np.arange(3)
+    bar1 = plt.bar(t, fruit[0], 0.5, color='red')
+    bar2 = plt.bar(t, fruit[1], 0.5,
+                   color='yellow', bottom=fruit[0])
+    bar3 = plt.bar(t, fruit[2], 0.5, color='#ff8000',
+                   bottom=fruit[0]+fruit[1])
+    bar4 = plt.bar(t, fruit[3], 0.5, color='#ffe5b4',
+                   bottom=fruit[0]+fruit[1]+fruit[2])
+    plt.ylabel('Quantity of Fruit')
+    plt.title('Number of Fruit per Person')
+    plt.xticks(t, ('Farrah', 'Fred', 'Felicia'))
+    plt.yticks(np.arange(0, 81, 10))
     plt.ylim(0, 80)
-    plt.xlabel("", fontsize='x-small')  # ensures no label misalignment
-    plt.title("Number of Fruit per Person", fontsize='x-small')
-    plt.legend(fontsize='x-small')
+    plt.legend((bar1, bar2, bar3, bar4),
+               ('apples', 'bananas', 'oranges', 'peaches'))
+    plt.tight_layout()
     plt.show()
+
+
+bars()
