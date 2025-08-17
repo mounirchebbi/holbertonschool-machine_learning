be42f2fc9cf93de57d805d29a789a6020fb05825
mounirchebbi <2157@holbertonschool.com>
2025-08-17 14:42:50 +0100

    stacked bar

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,21 +8,20 @@ def bars():
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))  # 4 fruits x 3 people
     people = ['Farrah', 'Fred', 'Felicia']
-    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']  # apples, bananas, oranges, peaches
     fruits = ['Apples', 'Bananas', 'Oranges', 'Peaches']
-
-    # Bottom positions for stacking
-    bottoms = np.zeros(fruit.shape[1])
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
     plt.figure(figsize=(6.4, 4.8))
+    bottoms = np.zeros(fruit.shape[1])
 
-    for i in range(fruit.shape[0]):  # Loop through each fruit
+    for i in range(fruit.shape[0]):
         plt.bar(people, fruit[i], bottom=bottoms, color=colors[i], width=0.5, label=fruits[i])
-        bottoms += fruit[i]  # Update bottom for next fruit
+        bottoms += fruit[i]
 
-    plt.ylabel("Quantity of Fruit")
+    plt.ylabel("Quantity of Fruit", fontsize='x-small')
+    plt.yticks(np.arange(0, 81, 10), fontsize='x-small')
     plt.ylim(0, 80)
-    plt.yticks(np.arange(0, 81, 10))
-    plt.title("Number of Fruit per Person")
-    plt.legend()
+    plt.xlabel("", fontsize='x-small')  # ensures no label misalignment
+    plt.title("Number of Fruit per Person", fontsize='x-small')
+    plt.legend(fontsize='x-small')
     plt.show()
